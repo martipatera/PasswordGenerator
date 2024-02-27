479129d0c2a29793393ca3e0a6dba57673e10a1e
martipatera <martinaretap@gmail.com>
2024-02-27 17:04:34 +0100

    while loop uz funguje

--- Program.cs
+++ Program.cs
@@ -6,65 +6,66 @@ namespace PasswordGenerator
         static void Main(string[] args)
         {
             string input;
-            
-                Console.Clear();
+            while (true) /* podminku nastavime aby byla vzdy pravdiva = je nekonecna
+                          * kdyz podminka if bude pravdiva, hodi se to do switche a ukonci se while loop pomoci break 
+                          * kdyz zadam cokoliv jineho bude se to opakovat protoze v else nemam break*/
+
+            {
+                
                 Console.WriteLine("-------------------------------------------------------------");
-                Console.WriteLine("Please select  the password strength:");
+                Console.WriteLine("Please select the password strength:");
                 Console.WriteLine("1 or + for only characters");
-                Console.WriteLine("2 or ě for charactects + numbers");
-                Console.WriteLine("3 or š for charactects + numbers + special symbols");
+                Console.WriteLine("2 or ě for characters + numbers");
+                Console.WriteLine("3 or š for characters + numbers + special symbols");
                 Console.WriteLine("k for end");
                 Console.WriteLine("-------------------------------------------------------------");
 
                 string pressedKey = Console.ReadLine();
                 input = pressedKey.ToLower();
                 Console.Clear();
-                if (input.Length <= 0 || input.Length > 1 || input == " ")
 
-                {
-                    Console.WriteLine("Wrong Input");
-                    Console.Write(input);
-
-                }
-                else
+                if (input == "1" || input == "+" || input == "2" || input == "ě" || input == "3" || input == "š" || input == "k")
                 {
                     switch (input)
-
                     {
                         case "1":
                         case "+":
-
                             Console.WriteLine("Pressed " + input);
-                            break;
-
-
-
+                            break; //ukonceni switche, musi byt!!!
 
                         case "2":
                         case "ě":
                             Console.WriteLine("Pressed " + input);
-                            break;
-
-
+                            break; //ukonceni switche, musi byt!!!
 
                         case "3":
                         case "š":
                             Console.WriteLine("Pressed " + input);
-                            break;
+                            break; //ukonceni switche, musi byt!!!
 
+
+                        case "k":
+                            Console.WriteLine("END OF PROGRAM");
+                            break; //ukonceni switche, musi byt!!!
 
                         default:
-                            break;
-
+                            break; //ukonceni switche, musi byt!!!
                     }
 
-
+                    break; // ukonceni nekonecne smycky smycky po zadani platneho inputu
                 }
+                else //bude se opakovat dokud nezadam spravnou podminku if
+                {
 
+                    Console.WriteLine("WRONG INPUT PLEASE TRY AGAIN");                   
+                }
+            }
 
-
-            
 
         }
+
+
     }
+
+    
 }
